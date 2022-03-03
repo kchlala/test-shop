d5542452e0e752be618d00eb8ecd9f9e038c7766
kchlala <kocharlilala1@gmail.com>
2022-03-03 11:15:52 +0400

    first

---
+++ Server/config/MongoDb.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDatabase = async () =>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL,{
+            useUnifiedTopology:true,
+            useNewUrlParser:true
+        })
+        console.log('MongoDB Connected')
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        process.exit(1);
+    }
+}
+export default connectDatabase;
---
+++ Server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDatabase from './config/MongoDb.js';
+import ImportData from "./DataImport.js";
+import productRoute from './Routes/ProductRoutes.js'
+dotenv.config();
+connectDatabase();
+const app=express();
+
+//API
+app.use("/api/import",ImportData)
+app.use("/api/products",productRoute)
+
+
+app.get("/",(req,res)=> {
+    res.send("Api is running")
+})
+const PORT=process.env.PORT || 1000;
+app.listen(PORT,console.log(`server running port ${PORT}`))
